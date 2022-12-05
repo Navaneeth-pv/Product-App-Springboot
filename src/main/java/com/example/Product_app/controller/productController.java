23d9fbc9b2c887e644a61fbccbdfbba34d2b8c86
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-05 21:32:54 +0530

    home page

--- src/main/java/com/example/Product_app/controller/productController.java
+++ src/main/java/com/example/Product_app/controller/productController.java
@@ -1,8 +1,14 @@
 package com.example.Product_app.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class productController {
+
+    @PostMapping("/")
+    public String Homepage(){
+        return  "Welcome to Home page";
+    }
 
 }
